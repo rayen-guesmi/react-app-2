49a089ca8c3d91b8279b93733149c38273b8b30c
Your Name <guesmirayen9@gmail.com>
2021-11-02 19:44:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import './assets/css/style.css'; 
+// import img1 from './img/rh.jpg';
+import img2 from './components/img2.jpg'
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div  classname="title">
+     <h1> Project X </h1>
+     <div className="img">
+     <img className="img1" src='./img/rh.jpg' alt="" />
+     <img className="img2" src ={img2} alt=""/>
+     </div>
+     </div>
   );
 }
 
